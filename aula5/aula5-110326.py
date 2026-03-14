2fe1973359f2e615d86f83d890bbc7a8da7abba3
diogomedina-icloud <diogodurao10@gmail.com>
2026-03-13 21:19:53 -0300

    atualizando

--- aula5/aula5-110326.py
+++ aula5/aula5-110326.py
@@ -1,12 +1,74 @@
 #laços de repetiçao
 
-intervalo1 = range(10)
-print(intervalo1)
+# intervalo1 = range(10)
+# print(intervalo1)
 
 
-print('***')
+# print('***')
 
 
-for numero in range(10):
-    print('numero:')
-    print(numero)
+# for numero in range(1,10,2):
+#     print("CONTANDO")
+#     print('numero:')
+#     print(numero)
+
+
+# for i in range(5):
+#     try:
+#         print(f"numero {i + 1} de 5 :")
+#         num = float(input("digite um numero:"))
+
+
+#         dobro = num * 2
+#         triplo = num * 3
+#         quadruplo = num * 4
+
+#         print(f" Resultado: dobro={dobro}, triplo = {triplo}, quadruplo = { quadruplo} \n")
+#     except ValueError:
+#         print("Entrada inválida. Tente novamente.")
+
+# WHILE>>>>>
+
+# loolap=float(input('qual o valor do evento?'))     # dependendo do input cai num loop infinito 
+
+# while loolap > 300:
+#     print('mas não vou mesmo!!!')
+    
+
+# print('Estarei la!')
+
+
+
+# >>> WHILE
+
+loolap=float(input('Qual o valor do evento (inteira)?')) #Dependnedo do input, cai num loop infinito
+
+while loolap > 300:
+    print('Mas não vou mesmo!!!')
+
+print('Estarei lá!')
+##
+
+loolap=0
+
+while loolap > 300:
+    loolap=float(input('Qual o valor do evento (inteira)?')) #Dependnedo do input, cai num loop infinito
+    print('Mas não vou mesmo!!!')
+
+print('Estarei lá!')
+##
+
+loolap=300
+
+while loolap >= 300:
+    loolap=float(input('Qual o valor do evento (inteira)?')) #Dependnedo do input, cai num loop infinito
+    print('Mas não vou mesmo!!!')
+
+print('Estarei lá!')
+
+
+
+
+
+
+
